ietf-wg-bpf/ebpf-docs

commit e1d0f4de05ed7149aa12b6b1a6d2c0a6eafe56f4

tools/ebpf-check: Parse description when generating list of instructions
In the future, we will likely stop to parse the Appendix table from the
RST document to dynamically generate the list of instructions, and will
instead have a reference JSON committed into the repository.

In preparation for that, let's add the description and category of each
instruction to the generated JSON, to help human read and fix it in the
future if necessary.

Head of the generated file with the current RST:

    [
      {
        "opc": 0,
        "src": 0,
        "imm": "any",
        "dsc": "(additional immediate value)",
        "cat": "64-bit immediate instructions"
      },
      {
        "opc": 4,
        "src": 0,
        "imm": "any",
        "dsc": "dst = (uint32_t)(dst + imm)",
        "cat": "Arithmetic instructions"
      },
      ...
    ]

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

tools/ebpf-check/docs-to-ops.py
@@ -7,7 +7,7 @@ import re
 
 __hex = '0[xX][0-9a-fA-F]+'
 HEX_RE = re.compile(__hex)
-OP_RE = re.compile(f'^({__hex})\s+(any|{__hex})\s+(any|{__hex}).*')
+OP_RE = re.compile(f'^({__hex})\s+(any|{__hex})\s+(any|{__hex})\s+(.*\S)\s+`(.*)`_$')
 
 class OpNotFound(BaseException):
     pass
@@ -36,11 +36,32 @@ class Parser(object):
         if not capture:
             raise OpNotFound
 
-        self.ops.append({
+        insn = {
             'opc' : hex_to_dec(capture.group(1)),
             'src' : hex_to_dec(capture.group(2)),
             'imm' : hex_to_dec(capture.group(3))
-        })
+            'dsc' : capture.group(4),
+            'cat' : capture.group(5),
+        }
+
+        # Descriptions spans multiple lines.
+        # Parse them, and seek back when meeting next instruction.
+        if insn['dsc'] == 'lock::':
+            while True:
+                pos = self.reader.tell()
+                line = self.reader.readline()
+                if not line:
+                    break
+                if line == '\n':
+                    continue
+                capture = OP_RE.match(line)
+                if not capture:
+                    insn['dsc'] += '; ' + line.strip()
+                else:
+                    self.reader.seek(pos)
+                    break
+
+        self.ops.append(insn)
 
     def run(self):
         self.seek_to('Appendix',

commit 40f300a928ca6392f23899a8ff3ae0afa1523b17

tools/ebpf-check: Fix missing comma in docs-to-ops.py
Python legitimately complains about a missing comma, let's fix it.

Fixes: e1d0f4de05ed ("tools/ebpf-check: Parse description when generating list of instructions")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

tools/ebpf-check/docs-to-ops.py
@@ -39,7 +39,7 @@ class Parser(object):
         insn = {
             'opc' : hex_to_dec(capture.group(1)),
             'src' : hex_to_dec(capture.group(2)),
-            'imm' : hex_to_dec(capture.group(3))
+            'imm' : hex_to_dec(capture.group(3)),
             'dsc' : capture.group(4),
             'cat' : capture.group(5),
         }